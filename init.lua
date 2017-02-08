d997005fe107edffd4e8f447110a1bf6d1036648
SB <steffibrinskelle6@gmail.com>
2017-02-08 10:23:42 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
+print("++++++++ Hello from s +++++++++")
+minetest.after(5,
+	function(params)
+		minetest.chat_send_all("Don't Forget To Have a Taco!")
 --minetest.register_node("tutorial:decowood", {
 	tile_images = {"tutorial_decowood.png"},
 	groups={level=1},
 })
-minetest.register_craftitem("farming:donut", {
-	description = S("Donut"),
+minetest.register_craftitem("Taco", {
+	description = S("Taco"),
 	inventory_image = "farming_donut.png",
 	on_use = minetest.item_eat(5),
 })
