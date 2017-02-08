a17420afef760e4d3d41e2b2927e9d54c1cfcadd
SB <steffibrinskelle6@gmail.com>
2017-02-08 10:09:26 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
-minetest.register_node("tutorial:decowood", {
+--minetest.register_node("tutorial:decowood", {
 	tile_images = {"tutorial_decowood.png"},
 	groups={level=1},
+})
+minetest.register_craftitem("farming:donut", {
+	description = S("Donut"),
+	inventory_image = "farming_donut.png",
+	on_use = minetest.item_eat(5),
+})
+
+minetest.register_craft({
+	output = "taco 3",
+	recipe = {
+		{'', 'farming:wheat', ''},
+		{'farming:wheat', '', 'farming:carrot'},
+		{'', 'farming:wheat', ''},
+	}
 })
